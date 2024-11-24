1466c331396b5bcf8de75341924a866475fa191c
Harshit <harshit242003@hotmail.com>
2024-11-24 16:28:17 +0530

    Initial Commit

---
+++ ARDiceeTests/ARDiceeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARDiceeTests.swift
+//  ARDiceeTests
+//
+//  Created by Harshit ‎ on 11/24/24.
+//
+
+import Testing
+@testable import ARDicee
+
+struct ARDiceeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
